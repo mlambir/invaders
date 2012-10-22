2ec8996686adf19d9d52470d5f111c68ba2386d5
Mariano Lambir <mlambir@gmail.com>
2012-10-22 16:53:50 -0300

    cambie fps del juego

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function start_game(){
-    jaws.switchGameState(MainGameState);
+    jaws.switchGameState(MainGameState, {fps:50});
 }
 
 jaws.start(ShowTextState, {}, {title:"Fiqus Invaders!", other:"apreta espacio para iniciar"});
